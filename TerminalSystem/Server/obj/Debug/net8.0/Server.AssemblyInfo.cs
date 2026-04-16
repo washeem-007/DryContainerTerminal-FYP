7c150387e1dec36186c84217d9f6d6f08b2d9370
washeem-007 <mohamed.20241685@iit.ac.lk>
2026-04-16 20:26:28 +0530

    UI changes

--- TerminalSystem/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ TerminalSystem/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dab0ea2e1ecf7e7346928db0f2ee51bbb9d8b580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec064ded6e01852d4fe597626202066bc49378d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
